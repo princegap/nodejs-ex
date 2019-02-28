6315c6dab0fbd76ca554aee04488ec504e5df33f
Prince Aiyathurai <princegap@gmail.com>
2019-02-28 23:39:40 +0530

    Redis config removed

--- server.js
+++ server.js
@@ -56,16 +56,6 @@ if (mongoURL == null) {
   }
 }
 
-var redis = require('redis');
-var client = redis.createClient('10.130.2.163','6379');
-
-client.on('connect', function() {
-    console.log('Redis client connected');
-});
-
-client.on('error', function (err) {
-    console.log('Something went wrong ' + err);
-});
 
 var db = null,
     dbDetails = new Object();
